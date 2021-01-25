d3351ec68f115fc8631ae328e5752de5cda17cb9
[Sofia Vega] <[sofia.vega18@gmail.com]>
2021-01-24 23:08:52 -0700

    Add prices to products

--- scripts/script1.py
+++ scripts/script1.py
@@ -19,13 +19,16 @@ def find_cell(sheet, val):
 for i in range(1,24):
     sheet = book.sheet_by_index(i)
     category=sheetindex.cell_value(i-1,1)
-    linea="{ \"id\": \""+sheet.name+"\","+"\"nombre\": \""+sheet.name+"\","+"\"categoria\": \""+str(category)+"\","
+    telefono=sheetindex.cell_value(i-1,2)
+    linea="{ \"id\": \""+sheet.name+"\","+"\"nombre\": \""+sheet.name.lower()+"\","+"\"categoria\": [\""+str(category).lower()+"\"],"+"\"telefono\": \""+str(telefono).lower()+"\","
     listaproductos=[]
     col, row=find_cell(sheet, "productos especificos")
     print (col,row)
     row=row+1
     while(row<sheet.nrows):
-        listaproductos.append("\""+str(sheet.cell_value(row,col))+"\"")
+        category=str(sheet.cell_value(row,col-1)).lower()
+        cantidad=str(sheet.cell_value(row,col+1))
+        listaproductos.append("{ producto: \""+str(sheet.cell_value(row,col))+"\", precio: \""+str(sheet.cell_value(row,col+2))+"\", categoria: \""+category+"\", cantidad: \""+cantidad+"\" }")
         row=row+1
     productos="\"productos\": ["+", ".join(listaproductos) + "]},"
     linea=linea+productos
